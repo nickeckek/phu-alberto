7c300d44013f9b0f5a01c4df371249576f9c99d5
Krystian <krystian.kramek1@gmail.com>
2026-03-06 17:49:33 +0100

    zmiana output na serwer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
+
 import cloudflare from '@astrojs/cloudflare';
 
+// https://astro.build/config
 export default defineConfig({
-  // To mówi Astro, żeby generowało kod na serwer (Cloudflare), a nie tylko pliki HTML
-  output: 'server', 
-  adapter: cloudflare(),
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: cloudflare()
 });
